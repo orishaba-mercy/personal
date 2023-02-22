7aab97e983922dcdbf6c983847ec5bb3f0b62de6
orishaba-mercy <orishabamercybanga@gmail.com>
2023-02-22 14:55:03 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+fun main(args: Array<String>) {
+    var c = 67
+    var d =5
+    var multiply = c*d
+    println("the multiply of two numbers is :"+ multiply)
+
+
+    var a =99
+    var b =4
+    var modules = 99%4
+    println("The modules of two numbers is :" +modules)
+
+
+    var t =77
+    var f = 26
+    var subtract =t-f
+    println("the subtract of two numbers is:"+subtract)
+
+    var n=84
+    var m=4
+    var division=n/m
+    println("the division of two numbers is:"+division)
+
+    var capitalcity = "kampala"
+    println("the captal city of uganda is :"+capitalcity
+
+    )
+
+
+
+    //You are creating an app to capture student records. Some of the data you will
+    //capture includes full name, age, phone number, weight in kg and citizen. For the
+    //citizen field you will track whether a student is a Kenyan or not.
+
+    var name = "orishaba mercy"
+    println("the name of student is:"+"name")
+
+    var age = 22
+    println("the age of student is:"+age)
+
+    var tell = +2567846778876
+    println("the tell of student is:"+tell)
+
+    var weight = 67.4
+    println("the weight of student is:"+weight)
+    var isugandan = true
+    println("the isugandan of student is:"+isugandan)
+
+
+
+
+
+
+
+}
